066c52de45ae3120ed954a5d2df11906673d30af
Tony Blum <tonyblum@me.com>
2016-02-02 11:49:13 -0800

    new dashboard with 5 recent products

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,20 @@
 ActiveAdmin.register_page "Dashboard" do
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Recent Products" do
+          table_for Product.order("id desc").limit(5) do
+            column :id
+            column "Product Title" , :title do |post|
+              link_to post.title , [:admin,post]
+            end
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+            column :category , :sortable => :category
+            column :created_at
+          end
+          strong (link_to "Show All Products", admin_products_path )
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
